be6a6e7437a62c9d415f1262c0bb84957f87c18f
Abhinav <abhinavsinghhg@gmail.com>
2023-10-31 20:51:36 +0530

    v1-heros4

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,7 @@ import Hero4 from "./hero-section4/hero-sect4";
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <Hero4 />
-      {/* hero-section-4-component */}
-    </>
-  );
+  return <Hero4 />;
 }
 
 export default App;
