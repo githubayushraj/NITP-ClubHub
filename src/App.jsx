18a937caef2ecdc8b15118e6326b3a6a54165e3e
Abhinav <abhinavsinghhg@gmail.com>
2023-10-31 20:46:39 +0530

    update in v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Hero4 from "./hero-section4/hero-sect4";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <p className='text-6xl font-bold text-pink-600 hover:text-sky-600 text-center pt-48'>REACT+TAILWIND SETUP</p>
+      <Hero4 />
+      {/* hero-section-4-component */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
