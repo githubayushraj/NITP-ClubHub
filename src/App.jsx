5f9e43c380f76ec9a6285eb66f2898f4122c3116
ritikkr066 <ritikkrsiwan345@gmail.com>
2023-11-04 22:20:18 +0530

    hero5

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Hero5 from './Hero5'
 import './App.css'
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
 
   return (
     <>
-      <p className='text-6xl font-bold text-pink-600 hover:text-sky-600 text-center pt-48'>REACT+TAILWIND SETUP</p>
+      {/* <p className='pt-48 text-6xl font-bold text-center text-pink-600 hover:text-sky-600'>REACT+TAILWIND SETUP</p> */}
+      
     </>
   )
 }
