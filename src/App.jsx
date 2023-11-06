8a1167a072c8c8dc1e0db7790cc933579b2e35d2
iamshobh <iamshobhraj@gmail.com>
2023-11-06 20:21:22 +0530

    added hero2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Hero2 from './hero2/hero2.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <p className='text-6xl font-bold text-pink-600 hover:text-sky-600 text-center pt-48'>REACT+TAILWIND SETUP</p>
+      <Hero2 />
     </>
   )
 }
