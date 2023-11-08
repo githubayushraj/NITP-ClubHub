9e8e95508eaebdc84a33e4a3c29bac4e1c964971
Prince <12prince345singh@gmail.com>
2023-11-09 00:29:43 +0530

    React-icon Dependency added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import Hero4 from "./hero-section4/hero-sect4";
-
+import Footer from './Footer/Footer'
 function App() {
   const [count, setCount] = useState(0);
-
-  return <Hero4 />;
+  
+  return(
+    <div>
+      <Hero4 />
+      <Footer />
+    </div>
+  )
 }
 
 export default App;
