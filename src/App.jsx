7be0227fb96084ab9f5fbe15f35a74b979168b31
iamaniket <iamaniket484@gmail.com>
2023-11-10 14:25:44 +0530

    added Hero3

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero4 from "./hero-section4/hero-sect4";
 import Footer from './Footer/Footer'
 import Hero2 from "./hero2/hero2.jsx";
 import Hero5 from "./Hero5.jsx";
+import Hero3 from "./hero3.jsx";
 import HomePage from "./pages/homePage.jsx";
 import Navbar from "./Navbar.jsx";
 function App() {
@@ -13,7 +14,8 @@ function App() {
     <Navbar/>
     <HomePage/>
       <Hero2 />
-       <Hero4 />;
+       <Hero4 />
+       <Hero3 />
        <Hero5/>
        <Footer />
     </>
---
+++ src/Hero3.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Hero3 = ()=>{
+    return(
+        <>
+                <div className="bg-gradient-to-br from-violet-950 to-fuchsia-950 flex md:flex-row flex-col justify-center md:w-full h-fit w-full py-12 overflow-x-hidden">
+            <div className="md:flex">
+               <div className="bg-white h-56 w-72 mx-12 md:h-auto md:w-2/5"></div>
+               <div className="md:flex md:flex-col">
+               <div className="font-bold md:text-5xl text-3xl text-white md:py-6 px-10 py-4">Exciting Fest Highlights Await</div>
+               <div className="text-zinc-400 text-xs md:text-base px-10">Join us for a thrilling celebration of Technology and culture at Corona and Melange, the Annual fest of NIT Patna.Experience wide range of events, workshops, performances, and more.</div>
+               
+               <div className="font-bold md:text-5xl text-3xl text-white md:py-6 px-10 py-4">Unforgettable Experiences Await</div>
+               <div className="text-zinc-400 text-xs md:text-base px-10">Get ready to be amazed by the incredible lineup of activities at Corona and Melange. From technical competitions to art exhibitions, there's something for everyone to enjoy..</div>
+               <div className="font-bold md:text-5xl text-3xl text-white md:py-6 px-10 py-4">Engaging Workshops and Talks</div>
+               <div className="text-zinc-400 text-xs md:text-base px-10">Expand your knowledge and skills through interactive workshops and insightful talks by industry experts. Don't miss the opportunity to learn from the best.</div>
+               </div>
+            </div>
+            </div>
+        </>
+    )
+}
+
+export default Hero3;
