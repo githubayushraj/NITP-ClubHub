f1b34d5174533588d167415b35357d4323a4b684
Manyachandra <chandramanya78@gmail.com>
2023-11-11 21:48:17 +0530

    Prev event

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+
+import Previous from "../components/previous";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <p className='text-6xl font-bold text-pink-600 hover:text-sky-600 text-center pt-48'>REACT+TAILWIND SETUP</p>
+  <>
+     
+{/* <About/>
+      
+      <Details/> */}
+      <Previous/>
+     
     </>
-  )
+    
+  );
 }
 
-export default App
+export default App;
